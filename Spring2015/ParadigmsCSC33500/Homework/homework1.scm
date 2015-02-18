fd3629b01c008ad68ab7dcee8059bd36698c58b2
Javier <quaere1verum@gmail.com>
2015-02-18 11:02:09 -0500

    Updating info

--- Spring2015/ParadigmsCSC33500/Homework/homework1.scm
+++ Spring2015/ParadigmsCSC33500/Homework/homework1.scm
@@ -15,32 +15,27 @@
 ;(/ (+ 5 (+ 4 (- 2 (- 3 (+ 6 (/ 4 5)))))) (* 3 (* (- 6 2) (- 2 7) )))
 
 ; 1.3
-(def sum_twolargest(a b c)
+#|(define sum_twolargest(a b c)
 	(+ (cond ((> a b) a)
 		(else b))
 	(cond ((> b  c) b)
 	(else c))
+	) )
+|#
+
+; 1.3
+(define (sumlargestsqrs a b c)
+	 (cond ((and (< a b) (< a c)) (squaresum b c))	
+		((and (< b a) (< b c)) (squaresum a c))
+		((and (< c a)(< c b)) (squaresum a b) )) )
+
+(define (squaresum x y)
+	(+ (* x x)(* y y))
 	)
 
-	)
+(sumlargestsqrs 1 2 4)
+
+
 
 ; iterative works in constant space 
 ; check difference between iterative and recursion
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Spring2015/ParadigmsCSC33500/Homework/hw1_pajuelo.scm
+++
@@ -1,8 +0,0 @@
-#lang scheme
-; test code 
-
-
-(define (square x) (* x x))
-
-(define (f3 b c) (cond((> f3 b) f3)
-			))
