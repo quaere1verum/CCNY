be4caec192feaf196e05f6a641d253b0a7d25f10
Javier <quaere1verum@gmail.com>
2015-02-18 21:09:50 -0500

    Updates

---
+++ Spring2015/ParadigmsCSC33500/Homework/homework3.scm
@@ -0,0 +1,38 @@
+
+
+; Third Homework Set
+; CSc 335
+; Spring 2015
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; This is a long problem set - you will want to allocate some hours.
+
+; Don't Fall Behind on the Homework!
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Review today's lecture notes before starting ...
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Proofs are required for all programs
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; 1.  Abelson and Sussman, Problems 1.11 and 1.12
+
+; 2.  Write iterative and recursive scheme functions to return the sum of the digits within
+; a non-negative integer.  For example, (sum-of-digits 345) is 12.
+
+; 3.  Write iterative and recursive scheme programs to test whether the digits in a non-negative
+; integer are in increasing order.  For example, the digits of 12348 are in increasing order, while
+; those of 12343 are not.
+
+; You may find the built-in functions quotient, remainder, truncate, zero? -- and perhaps others --
+; helpful as you design your solutions for problems 2 and 3.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
